05ddb2a01de790e6de0f1e2320f697e854f484fa
Bhuwanxhettri <you@example.com>
2023-05-02 22:43:27 +0545

    phone details

--- app/phones/page.tsx
+++ app/phones/page.tsx
@@ -4,7 +4,6 @@ import List from '@/components/Phones/List';
 const page = () => {
   return (
     <div>
-        
       <section className="relative z-10 overflow-hidden  pt-10">
           <List/>
       </section>
